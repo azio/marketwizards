7782041a9164e7eb19db97489923caf176a57c8e
Adam Bull <adam@marketwizards.io>
2018-10-09 15:41:52 +0100

    Bulked manifest files into all.manifest temporarily, will refactor individual exports later. i.e. only trans, only blocks etc

--- mwclient.sh
+++ mwclient.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# Author: Adam Bull
+# Date: 09/10/2018
+# Description: This script downloads the ethereum blockchain data
+# to presto-db hive worker nodes in a coordinator cluster 
+# or for use as a single presto-db co-ordinator and worker server
+# it saves users having to generate this CSV data themselves since
+# the only way to do that is to run an ethereum fullnode and RPC which is quite time consuming
+# and costs money. The goal of this project is to create a lightweight blockchain manager prototype for the means
+# of synchronising blockchain data easily and making it accessible to all
+
+
 # Retrieve ethereum-etl blockchain data
 # prototype for a larger system
 
@@ -7,12 +18,12 @@ sleep 2;
 
 # Directory ran locally at marketwizards for the process to create manifests
 cd ~
-wget http://repo.marketwizards.io/manifest/block.manifest -P .marketwizards/manifest
+wget http://repo.marketwizards.io/manifest/all.manifest -P .marketwizards/manifest
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
 
 filename="$line"
 dirname=${filename%/*}
 wget "http://repo.marketwizards.io/$line" -P ".marketwizards/$dirname"
-done < .marketwizards/manifest/block.manifest
+done < .marketwizards/manifest/all.manifest
 
