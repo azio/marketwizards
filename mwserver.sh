05fee914a71456daeb36bc903bdff21b86098aac
Adam Bull <adam@marketwizards.io>
2018-10-09 15:21:08 +0100

    shellscript server prototype for generating manifest files used by client to sync ethereum-etl data from a central repo

---
+++ mwserver.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# build manifest
+
+# local process to build manifest
+
+echo "Building Ethereum Blocks Manifest File"
+sleep 2
+find blocks/ -type f | grep csv | sort -n > manifest/block.manifest
+find contract_addresses/ -type f | sort -n > manifest/contract_addresses.manifest
+find contracts/ -type f | grep csv | sort -n > manifest/contracts.manifest
+find logs/ -type f | grep csv | sort -n > manifest/logs.manifest
+find receipts/ -type f | grep csv | sort -n > manifest/receipts.manifest
+find token_addresses/ -type f |  sort -n > manifest/token_addresses.manifest
+find tokens/ -type f | grep csv | sort -n > manifest/tokens.manifest
+find token_transfers/ -type f | grep csv | sort -n > manifest/token_transfers.manifest
+find transaction_hashes/ -type f | grep csv | sort -n > manifest/transaction_hashes.manifest
+find transactions/ -type f | grep csv | sort -n > manifest/transactions.manifest
+
